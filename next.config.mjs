88717c6c83df9bf61860935187de1e758fcc280b
Eric Xiao <73317463+NycRat@users.noreply.github.com>
2024-02-04 18:01:12 -0800

    fixed some github pages issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: "export",
+  basePath: "/new-personal-site",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
