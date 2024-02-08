dadf35d769b4faf3432bac7f6ecb0b33af345bdb
Eric Xiao <73317463+NycRat@users.noreply.github.com>
2024-02-07 20:17:46 -0800

    intro section improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Image from "next/image";
@@ -5,19 +6,70 @@ import AboutSection from "../components/about";
 import ProjectsSection from "../components/projects";
 import ContactSection from "../components/contact";
 import Fireworks from "../public/fireworks.webp";
+import Link from "next/link";
+import { useEffect } from "react";
+
+import { Montserrat } from "next/font/google";
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    let header = document.getElementById("header");
+
+    function update() {
+      if (header) {
+        if (window.scrollY >= window.outerHeight) {
+          header.style.setProperty("--header-opacity", "1");
+        } else {
+          header.style.setProperty(
+            "--header-opacity",
+            (
+              window.scrollY /
+              (window.outerHeight - header.offsetHeight)
+            ).toString(),
+          );
+        }
+      }
+    }
+
+    update();
+
+    window.addEventListener("scroll", update);
+  }, []);
+
   return (
     <main className="relative bg-bg1">
       <Header />
       <div id="home" className="relative h-screen">
-        <div className="w-1/4 text-right absolute right-0 m-14 top-1/4 text-lg">
-          {/* <h1 className="text-2xl"></h1> */}
-          {/* TODO - REWRITE THIS THINGY */}
-          My name is Eric; I am currently a student that has been programming
-          for 5 years. I have about 2 years of experience teaching programming
-          and also have experience doing full-stack web development on personal
-          projects for 2 years.
+        <span className={montserrat.className}>
+          <h1 className="text-9xl absolute bottom-12 left-12 mix-blend-difference font-bold">
+            Web Developer
+          </h1>
+        </span>
+        {/* <h1 className="text-9xl absolute top-1/2 left-1/2 font-bold text-[#fff8]">wha</h1> */}
+        <div className="text-right absolute right-14 top-24 text-lg">
+          <h1 className="text-6xl [text-shadow:_0_0_10px_#fff]">Eric Xiao</h1>
+          <span className="inline-block h-[1px] w-full bg-white"></span>
+          <span className="flex space-x-3 font-light text-3xl flex-col mt-6">
+            <Link
+              className="ml-auto hover:[text-shadow:_0_0_10px_#fff] transition-[text-shadow]"
+              href={"#about"}
+            >
+              About
+            </Link>
+            <Link
+              className="ml-auto hover:[text-shadow:_0_0_10px_#fff] transition-[text-shadow]"
+              href={"#projects"}
+            >
+              Projects
+            </Link>
+            <Link
+              className="ml-auto hover:[text-shadow:_0_0_10px_#fff] transition-[text-shadow]"
+              href={"#contact"}
+            >
+              Contact
+            </Link>
+          </span>
         </div>
         <Image
           src={Fireworks}
--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,10 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="p-4 border-slate-800 border-b-[1px] fixed w-full top-0 z-20 bg-[#0003] h-[70px]">
+    <header
+      className="p-4 border-slate-800 border-b-[1px] fixed w-full top-0 z-20 bg-[#23091466] h-[70px] opacity-[var(--header-opacity)]"
+      id="header"
+    >
       <span className="flex items-center m-auto w-fit">
         <Link
           href={"#home"}
