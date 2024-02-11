6afebb363d2fcb96ff511b6c237174413a535e56
Eric Xiao <73317463+NycRat@users.noreply.github.com>
2024-02-10 18:52:48 -0800

    updated meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Web Developer - Eric Xiao (NycRat)",
-  description: "i can make website :thumbsup",
+  description: "Eric Xiao personal website, web development portfolio, projects, services",
   authors: { name: "Eric Xiao", url: "https://github.com/NycRat" },
 };
 
