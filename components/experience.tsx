67c0120be2229c33095c5e9f6585980a34072cb0
Eric Xiao <ericxiao0000@gmail.com>
2024-11-09 21:52:35 -0800

    feat: experience page

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,5 +1,3 @@
-import Page from "@/components/page";
-
 interface ExperienceInfo {
   position: string;
   company: string;
@@ -9,38 +7,65 @@ interface ExperienceInfo {
 
 const experienceInfoList: ExperienceInfo[] = [
   {
-    position: "Teacher",
-    company: "",
-    dateRange: "",
-    descriptions: [],
+    position: "C++ Teacher",
+    company: "DC Coding Association",
+    dateRange: "Sep. 2022 - Mar. 2023",
+    descriptions: [
+      "Prepared course outline and taught weekly classes about C++",
+      "Taught without anyone else assisting, managed class of around 10+ students",
+      "Created, assigned, and marked take home assignments to strengthen student learning",
+      "Applied project-based learning for the focus of the course",
+    ],
   },
   {
-    position: "Teacher",
-    company: "",
-    dateRange: "",
-    descriptions: [],
+    position: "Arduino Workshop Instructor",
+    company: "DC Coding Association",
+    dateRange: "May 2024",
+    descriptions: [
+      "Created project-based workshop teaching basics of electronics and Arduino",
+      "Worked together with two other instructors to teach students",
+      "Taught groups of 8-12 students",
+      "Taught students of various ages (6-13)",
+      "Taught both programming and electronic wiring",
+    ],
   },
   {
-    position: "Teacher",
-    company: "",
-    dateRange: "",
-    descriptions: [],
+    position: "Science / Programming Teacher",
+    company: "BC Children and Youth Connection Society",
+    dateRange: "Sep. 2022 - Present",
+    descriptions: [
+      "Prepared and taught weekly classes based on BC Science 8 curriculum",
+      "Prepared and taught weekly classes based on self-created curriculum on various programming languages (C++/ Java)",
+      "Taught students from various ages (8-16)",
+      "Taught class of around 10+ students",
+      "Explored many methods of teaching, slideshow, teach with examples, project based learning",
+    ],
   },
 ];
 
-export default function ExperienceSection() {
+function Experience(props: ExperienceInfo) {
   return (
-    <Page className="bg-gradient-to-b from-bg2 to-bg1" id="experience">
-      {/* <h1 className="text-center text-6xl font-bold">Experience</h1> */}
-      <div className="grid grid-cols-3">
-        {experienceInfoList.map((info, i) => (
-          <Experience key={i} {...info} />
+    <div className="text-center p-8">
+      <h2 className="text-3xl font-black">{props.position}</h2>
+      <div className="text-1xl font-light">{props.company}</div>
+      <br />
+      <div>
+        {props.descriptions.map((a, i) => (
+          <div key={i}>{a}</div>
         ))}
       </div>
-    </Page>
+    </div>
   );
 }
 
-function Experience(props: ExperienceInfo) {
-  return <div>{props.position}</div>;
+export default function ExperienceSection() {
+  return (
+    <div id="experience" className="bg-gradient-to-b from-bg2 to-bg1 pt-8">
+      <h1 className="text-center text-6xl font-light">Experience</h1>
+      {/* <div className="grid grid-rows-3 md:grid-rows-1 md:grid-cols-3 gap-[1px] bg-white"></div> */}
+      {experienceInfoList.map((info, i) => (
+        <Experience key={i} {...info} />
+      ))}
+    </div>
+  );
 }
